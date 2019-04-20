59376208969c095f5bbe3da36079176c4cdc1e68
Isis Lovecruft <isis@patternsinthevoid.net>
2019-04-20 20:19:06 +0000

    Turn the crate into a library.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! Smooth Projective Hash Functions using curve25519-dalek
+//!
+//! # Examples
+//!
+//! ```
+//! # TODO (if you put code here it'll run as a test and also show up
+//! #       in the crate-level documentation!)
+//! ```
+
+mod sphf;
+
+pub use sphf::*;
